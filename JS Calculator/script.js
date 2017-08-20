a7957a2d84320ede02dfcf916ba143096d49c998
ramazan793 <demonmail.ru00@gmail.com>
2017-08-20 22:41:31 +0300

    Completed Calculator(Its like IOS calculator)

--- JS Calculator/script.js
+++ JS Calculator/script.js
@@ -1,28 +1,52 @@
 var currentnum = 0;
 var lastoperation;
+var opnum = 0;
+var mode = false;
 $(document).ready(function() {
     $('.num').click(function() {
-        $('#screen').append($(this).text());
+        if (mode) {
+            $('#screen').empty();
+            $('#screen').append($(this).text());
+            mode=false;
+        } else {
+            $('#screen').append($(this).text());
+        }
     });
-    $('#add').click(function() {
-        currentnum = Number($('#screen').html());
-        lastoperation = "add";
-        $('#screen').text(null);
-    });
-    $('#substract').click(function() {
-        currentnum = Number($('#screen').html());
-        lastoperation = "substract";
-        $('#screen').text(null);
-    });
-    $('#multiply').click(function() {
-        currentnum = Number($('#screen').html());
-        lastoperation = "multiply";
-        $('#screen').text(null);
-    });
-    $('#divide').click(function() {
-        currentnum = Number($('#screen').html());
-        lastoperation = "divide";
-        $('#screen').text(null);
+    $('#add,#substract,#multiply,#divide').click(function() {
+        if (opnum > 0) {
+            mode=true;
+            switch (lastoperation) {
+                case "add":
+                    currentnum += Number($('#screen').html());
+                    $('#screen').text(currentnum);
+                    lastoperation = "add";
+                    break;
+                case "substract":
+                    currentnum -= Number($('#screen').html());
+                    $('#screen').text(currentnum);
+                    lastoperation = "substract";
+                    break;
+                case "multiply":
+                    currentnum *= Number($('#screen').html());
+                    $('#screen').text(currentnum);
+                    lastoperation = "multiply";
+                    break;
+                case "divide":
+                    currentnum /= Number($('#screen').html());
+                    $('#screen').text(currentnum);
+                    lastoperation = "divide";
+                    break;
+                default:
+                    break;
+            }
+            lastoperation = $(this).attr("id");
+        } else {
+            currentnum = Number($('#screen').html());
+            lastoperation = $(this).attr("id");
+            $('#screen').text(null);
+            mode=false;
+        }
+        opnum++;
     });
     $('#equality').click(function() {
         switch (lastoperation) {
@@ -48,11 +72,15 @@ $(document).ready(function() {
                 break;
             default:
                 break;
-
         }
+        opnum=0;
+        mode=false;
     });
     $('#clear').click(function() {
         currentnum = 0;
+        mode=false;
+        opnum=0;
+        lastoperation = null;
         $('#screen').text(null);
     });
 });
