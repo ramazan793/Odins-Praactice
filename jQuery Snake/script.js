dbcedc8981a55ef18c3b36e60df8bac17056de89
ramazan793 <demonmail.ru00@gmail.com>
2017-08-25 17:39:02 +0300

    Add coords syst. and square class

--- jQuery Snake/script.js
+++ jQuery Snake/script.js
@@ -1,18 +1,56 @@
+function matrixArray(rows, columns) {
+    var arr = new Array();
+    for (var i = 0; i < columns; i++) {
+        arr[i] = new Array();
+        for (var j = 0; j < rows; j++) {
+            arr[i][j] = i + j + 1;
+        }
+    }
+    return arr;
+}
+class Square{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.block = document.createElement('div');
+    }
+    get(){
+        return this.block;
+    }
+}
+
 $(document).ready(function() {
-    var s_wid = $('#gamescreen').width();
     var size = 40;
     var bordersize = 1;
+    var s_wid = $('#gamescreen').width();
     var mywid = s_wid * 1 / size - bordersize * 2 + "px";
     var myhei = mywid;
-    console.log(s_wid + " " + mywid);
-    for (var i = 1; i <= size * size; i++) {
-        var div = document.createElement('div');
-        $(div).addClass('square');
-        $('#gamescreen').append(div);
+    var grid = matrixArray(40, 40);
+
+    function field() {
+        for (var y = 0; y < size; y++) {
+            for (var x = 0; x < size; x++) {
+                grid[x][y] = new Square(x, y);
+                var div = grid[x][y].get();
+                $(div).addClass('square');
+                $('#gamescreen').append(div);
+                if (x == 19 && y == 19) {
+                    $(div).append("&Omicron;");
+                }
+            }
+        }
+        $('.square').css({
+            "border": bordersize + "px solid gray",
+            "width": mywid,
+            "height": myhei,
+            "text-align":"center"
+        });
     }
-    $('.square').css({
-        "border":bordersize+"px solid gray",
-        "width": mywid,
-        "height": myhei
-    });
+
+    function render() {
+        field();
+    }
+
+    render();
+
 });
